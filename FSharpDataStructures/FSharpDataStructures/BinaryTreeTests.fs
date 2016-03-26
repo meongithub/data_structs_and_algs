d4c6a78e078c8c9f6ede2bfdf0fbb8a654b062f3
meonbitbucket <dpaudel@automationofthings.com>
2016-03-26 18:00:21 -0400

    added binary tree

---
+++ FSharpDataStructures/FSharpDataStructures/BinaryTreeTests.fs
@@ -0,0 +1,18 @@
+﻿module BinaryTreeTests
+
+open BinaryTree
+
+(*
+     2
+    / \
+   1   4
+  / \
+ 3  5
+
+*)
+let binTree1 = 
+    BinaryTree.Node(2, 
+                    BinaryTree.Node(1, 
+                                    BinaryTree.Node(3, Empty, Empty), 
+                                    BinaryTree.Node(5, Empty, Empty)), 
+                    BinaryTree.Node(4, Empty, Empty))
